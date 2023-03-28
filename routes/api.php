6a32b40ed80a72915712d99a3f5d84c92d444f19
ViniciusQueirozVQ <viniciusqueiroz.voq@gmail.com>
2023-03-28 19:50:35 -0300

    feat: add rotas

--- routes/api.php
+++ routes/api.php
@@ -14,9 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('api')->get('/user', function (Request $request) {
-    return $request->user();
-});
 
 
 Route::post('login', function (Request $request) {
@@ -35,4 +32,30 @@ Route::post('login', function (Request $request) {
     ]);
 
 });
+
+Route::middleware('api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::middleware('api')->group(function () {
+
+    Route::apiResources([
+       'disciplinas' => DisciplinaController::class,
+       'objetos3d' => Objeto3dController::class,
+       'aulas' => AulaController::class,
+    ]);
+ 
+    Route::get('objetos3d/{objeto_3d}/download', [Objeto3dController::class, 'download'])->name('objetos3d.download');
+ 
+ });
+ 
+ //API ROUTES FOR APP MOBILE
+ 
+ Route::prefix('mobile')->group(function () {
+ 
+    Route::get('/aulas/{codigo}',[AulaController::class, 'getAula'])->name('mobile.aulas.index');
+    Route::get('/objetos3d/{codigo}', [Objeto3dController::class, 'getObjeto3d'])->name('mobile.objetos3d.show');
+    Route::get('/objetos3d/{codigo}/download', [Objeto3dController::class, 'downloadObjeto3d'])->name('mobile.objetos3d.download');
+ 
+ });
 
